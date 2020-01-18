512ba7439df3f7b7e32de6735d5825c542680ffe
Martin Kennish <billthemaxster@gmail.com>
2020-01-18 22:16:15 +0000

    Get webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,25 @@ module.exports = {
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
+  module: {
+    rules: [
+        {
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: "ts-loader"
+                }
+            ]
+        },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+            enforce: "pre",
+            test: /\.js$/,
+            loader: "source-map-loader"
+        }
+    ]
+},
   output: {
     filename: 'bundle.js'
   },
