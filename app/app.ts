5cbf752160dcd5fcc687ae3464b9f65a86dd3f1a
Martin Kennish <billthemaxster@gmail.com>
2020-01-18 22:13:54 +0000

    Add TSConfig file and move to using separate compiled folder

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 function startGame() {
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+    let messagesElement = document.getElementById('messages');
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-document.getElementById('startGame').addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
