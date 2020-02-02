cc3b63808c6f6eea1f9f31b0acaa0e8e39ed6e71
Martin Kennish <billthemaxster@gmail.com>
2020-02-02 16:26:53 +0000

    Convert game to use TS Modules

--- app/utility.ts
+++ app/utility.ts
@@ -1,7 +1,10 @@
-class Utility {
-
-    static getInputValue(elementId: string): string {
-        const inputElement: HTMLInputElement = document.getElementById(elementId) as HTMLInputElement
-        return inputElement.value;
-    }
+function getInputValue(elementId: string): string {
+    const inputElement: HTMLInputElement = document.getElementById(elementId) as HTMLInputElement
+    return inputElement.value;
 }
+
+function logger(message: string): void {
+    console.log(message)
+}
+
+export { getInputValue as getValue, logger }
