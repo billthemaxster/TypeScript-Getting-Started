85df86a0840bcaefe62d97feecc8a47b028429f3
Martin Kennish <billthemaxster@gmail.com>
2020-02-02 13:58:16 +0000

    Switch back to tsc for compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  entry: './app/app.ts',
-  devtool: 'inline-source-map',
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  module: {
-    rules: [
-        {
-            test: /\.ts(x?)$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: "ts-loader"
-                }
-            ]
-        },
-        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        {
-            enforce: "pre",
-            test: /\.js$/,
-            loader: "source-map-loader"
-        }
-    ]
-},
-  output: {
-    filename: 'bundle.js'
-  },
+  entry: './js/app.js',
+//   devtool: 'inline-source-map',
+//   resolve: {
+//     extensions: [ '.tsx', '.ts', '.js' ]
+//   },
+//   module: {
+//     rules: [
+//         {
+//             test: /\.ts(x?)$/,
+//             exclude: /node_modules/,
+//             use: [
+//                 {
+//                     loader: "ts-loader"
+//                 }
+//             ]
+//         },
+//         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+//         {
+//             enforce: "pre",
+//             test: /\.js$/,
+//             loader: "source-map-loader"
+//         }
+//     ]
+// },
+//   output: {
+//     filename: 'bundle.js'
+//   },
   devServer: {
     inline: false
   }
